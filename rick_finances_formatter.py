45b6748795a1ea194109545f1cc62b52e4ae27e4
jrrlucenafilho <jrrlucenafilho@gmail.com>
2023-09-03 21:35:29 -0300

    Changed main comment

--- rick_finances_formatter.py
+++ rick_finances_formatter.py
@@ -1,8 +1,6 @@
 '''
 ---------------------------------------------------------------------------------------------------
 Reworked version of the calc conversion-and-formatting tool made to help accountants with finances handling/formatting.
-
-Obs: Planned to only work on UNIX-based systems
 ---------------------------------------------------------------------------------------------------
 '''
 
